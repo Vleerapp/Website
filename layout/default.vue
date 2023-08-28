dbee171ec8885f3d30870f4304fa8ebf9ff12264
Waradu <waradu@outlook.com>
2023-08-28 18:56:41 +0200

    created basic structure

---
+++ layout/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <slot />
+</template>
+
+<style lang="scss">
+</style>
---
+++ server/api/version.ts
@@ -0,0 +1,6 @@
+export default defineEventHandler((event) => {
+  return {
+    appVersion: "1.2.1",
+    webVersion: "1.0.0"
+  }
+})
