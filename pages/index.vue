20cba9d5948b5f87607023cfe156134d6aab5b85
PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2023-09-03 01:20:11 +0200

    added hero image

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,13 @@
 <template>
   <NuxtLayout>
-    <h1 class="title">Enjoy music again with Vleer</h1>
-    <h2 class="description">Discover a redefined way of listeing to music, all conveniently accessible right at your fingertips</h2>
-    <Download/>
+    <div class="hero">
+      <h1 class="title">Enjoy music again with Vleer</h1>
+      <h2 class="description">Discover a redefined way of listeing to music, all conveniently accessible right at your fingertips</h2>
+      <Download/>
+    </div>
+    <div class="img-border">
+      <img src="/hero.png" alt="" class="hero-image">
+    </div>
   </NuxtLayout>
 </template>
 
@@ -23,5 +28,23 @@
   margin-bottom: 32px;
   max-width: 370px;
   text-align: center;
+}
+
+.hero{
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 100px;
+}
+
+.hero-image{
+  max-width: 1185px;
+}
+
+.img-border{
+  padding: 8px;
+  border: 1px solid #544d5e6b;
+  border-radius: 30px;
 }
 </style>
