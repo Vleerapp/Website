2f0c719c7650c688a4e68f134fd9a171b61a3c16
PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2023-09-04 23:37:38 +0200

    added content to download page

--- pages/download.vue
+++ pages/download.vue
@@ -1,13 +1,127 @@
 <template>
-  <div>
-    <Navbar />
-  </div>
+  <NuxtLayout>
+    <div class="download-content">
+      <img src="/logo.webp" alt="" class="logo">
+      <div class="inner-content">
+        <h1 class="download-title">Download our App</h1>
+        <h2 class="download-description">Lighting fast Desktop experience on all platforms</h2>
+        <a href="">
+          <div id="button-outter" class="button-outter">
+            <div class="button-inner-page">
+              <img class="button-inner-logo" src="/apple.svg" alt="">
+              Download
+            </div>
+          </div>
+        </a>
+      </div>
+      <p class="tag">Compatible with Apple Silicon and Intel</p>
+    </div>
+  </NuxtLayout>
 </template>
 
-<script lang="ts" setup>
+<script setup lang="ts">
+let rotation = 90;
+let animationId: any = null;
 
+const startAnimation = async () => {
+  while (true) {
+    document.documentElement.style.setProperty("--rotation", rotation + "deg")
+    rotation += 1;
+    await new Promise(resolve => animationId = setTimeout(resolve, 7))
+    if (rotation >= 360) rotation = 0
+  }
+}
+
+onMounted(startAnimation)
+
+onUnmounted(() => {
+  clearTimeout(animationId);
+})
 </script>
 
+<style lang="scss">
+:root {
+  --rotation: 90deg;
+}
+
+.button-outter {
+  background: linear-gradient(var(--rotation), #ffffff 40%, #000000 60%);
+  width: 206px;
+  height: 50px;
+  border-radius: 12px;
+  padding: 1px;
+  position: relative;
+
+  .button-inner-page {
+    background-color: black;
+    width: 100%;
+    height: 100%;
+    border-radius: 11px;
+    display: flex;
+    flex-direction: row;
+    gap: 8px;
+    align-items: center;
+    justify-content: center;
+  
+    .button-inner-logo{
+      width: 24px;
+    }
+  }
+}
 
 
-<style></style>
+////////////////////////////
+
+.download-content {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  width: 100%;
+
+  .logo {
+    max-width: 260px;
+    height: auto;
+    border-radius: 260px;
+    margin-bottom: 48px;
+  }
+
+  .inner-content {
+    gap: 24px;
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    margin-inline: 32px;
+
+
+    .download-title {
+      font-weight: 400;
+      font-size: 48px;
+      font-family: Maax-Regular;
+    }
+
+    .download-description {
+      font-size: 16px;
+      font-family: Maax-Regular;
+      color: #ABABAB;
+    }
+  }
+
+  .tag {
+    margin: 16px;
+    font-size: 12px;
+    color: #ABABAB;
+  }
+}
+
+@media (max-height: 750px) {
+  .download-content {
+    position: relative;
+    top: 300px;
+  }
+}
+</style>
