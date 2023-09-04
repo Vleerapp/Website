c04162d15c1d570fc6b05bbb1c718102213785be
PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2023-09-04 23:37:14 +0200

    added content to about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,37 @@
 <template>
-  <div>
-    <Navbar/>
-  </div>
+  <NuxtLayout>
+    <div class="about-container">
+      <div class="about">
+        <h1 class="about-title">The Story of Vleer</h1>
+        <p class="text">Vleer was originally planned to just be a simple music app that can play music without having to pay for those Music subcriptions. But eventually it turned into a public project that we now build with your help to create a redefined way of how we listen to Music.<br><br>The direction of Vleer turned around by 180° and now we are here thogeter as the two founders of Vleer PandaDEV and Waradu.<br><br>The idea for Vleer or as we called it at the beginnning "Music Player" was from PandaDEV as he didn't like all the other Music apps as they are complicated to use and the main reason cost money which was a problem for him. So he decided to create one by himself that will work with the intention to be simple and free. Waradu the co-founder helped him because together it always works better.<br><br>Also in the main focus was that the app should be Open Source to create transparency and gain trust.</p>
+      </div>
+    </div>
+    <Ready />
+  </NuxtLayout>
 </template>
 
-<script lang="ts" setup>
+<style lang="scss">
+.about-container{
+  max-width: 1136px;
+  width: 100%;
+  margin-top: 80px;
 
-</script>
+  .about {
+    display: flex;
+    text-align: left;
+    align-items: left;
+    justify-content: left;
+    flex-direction: column;
+    gap: 16px;
+  
+    .text{
+      color: #bfbfbf;
+    }
 
-
-
-<style></style>
+    .about-title {
+      font-size: 48px;
+      text-align: left;
+    }
+  }
+}
+</style>
