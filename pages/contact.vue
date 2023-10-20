d1883c4aa8c8c340ab214975c381a878b586b898
PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2023-10-20 16:19:24 +0200

    added content to contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,16 @@
 <template>
   <NuxtLayout>
-
+    <div class="contact-container">
+      <div class="contact">
+        <h1 class="contact-title">Contact us</h1>
+        <p class="text">You can contact us on:</p>
+        <div class="tags">
+          <img src="xbanner.png" alt="xbanner" class="x banner">
+          <img src="discordbanner.png" alt="discordbanner" class="discord banner">
+        </div>
+        <p class="text">or via Email at <a href="mailto:contact@vleer.app">contact@vleer.app</a></p>
+      </div>
+    </div>
   </NuxtLayout>
 </template>
 
@@ -36,4 +46,50 @@ useHead({
 })
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.contact-container {
+  max-width: 1136px;
+  width: 100%;
+  margin-top: 80px;
+  margin-bottom: 32px;
+
+  .contact {
+    display: flex;
+    text-align: left;
+    align-items: left;
+    justify-content: left;
+    flex-direction: column;
+    gap: 16px;
+
+    .tags {
+      display: flex;
+      gap: 16px;
+    }
+
+    .x {
+      width: 230px;
+      height: 60px;
+    }
+
+    .discord {
+      width: 252px;
+      height: 60px;
+    }
+
+    .text {
+      color: #bfbfbf;
+    }
+
+    .contact-title {
+      font-size: 48px;
+      text-align: left;
+    }
+  }
+}
+
+@media (max-width: 700px) {
+  .contact-container{
+    margin-top: 40px;
+  }
+}
+</style>
