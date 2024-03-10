b4e963ea9b04887739dcb6948ac447ab430416f6
Waradu <waradu@outlook.com>
2024-03-10 20:32:23 +0100

    added base

--- app.vue
+++ app.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="content">
+    <div class="horizontal"></div>
+    <div class="horizontal"></div>
+    <div class="horizontal"></div>
+    <div class="horizontal down"></div>
+    <div class="horizontal down"></div>
+
+    <div class="vertical"></div>
+    <div class="vertical"></div>
+    <div class="vertical right"></div>
+    <div class="vertical right"></div>
+
+    <div class="title">
+      Vleer
+      <div class="dot"></div>
+      <div class="dot"></div>
+      <div class="dot"></div>
+      <div class="dot"></div>
+    </div>
+
+    <div class="description">
+      A Music app<br>from the Future
+      <div class="dot"></div>
+      <div class="dot"></div>
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+@use '~/assets/styles/main';
+</style>
