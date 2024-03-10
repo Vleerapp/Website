a64799909135f9023fde892451daa5a799b43a72
PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2024-03-10 12:21:23 +0100

    rewrite

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,34 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ["@nuxt/image", "nuxt-simple-sitemap", "@nuxtjs/robots"],
-  image: {
-    quality: 80,
-    format: ["webp"],
-    screens: {
-      xs: 320,
-      sm: 640,
-      md: 768,
-      lg: 1024,
-      xl: 1280,
-      xxl: 1536,
-      "2xl": 3072,
-    },
-  },
-  app: {
-    head: {
-      title: "Vleer",
-      script: [
-        ...(process.env.NODE_ENV === "production"
-          ? [
-              {
-                src: "https://plausible.pandadev.net/js/script.js",
-                defer: true,
-                "data-domain": "vleer.app",
-              },
-            ]
-          : []),
-      ],
-    },
-  },
-});
+  devtools: { enabled: true }
+})
