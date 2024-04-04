dc04409b3fa74b97e3691fe6b09420418575789f
PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2024-04-05 01:08:28 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false }
+  devtools: { enabled: false },
+  app: {
+    head: {
+      title: "Vleer",
+      script: [
+        ...(process.env.NODE_ENV === "production"
+          ? [
+              {
+                src: "https://plausible.pandadev.net/js/script.outbound-links.js",
+                defer: true,
+                "data-domain": "vleer.app",
+              },
+            ]
+          : []),
+      ],
+    },
+  },
 })
