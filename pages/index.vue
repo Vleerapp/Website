068d85a615650f566da1a3cfb1aba6f0b3f54cca
PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2024-05-06 14:33:51 +0200

    fixed image not clipping into the screen width

--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,22 @@
   <div class="hero">
     <div class="hero-text">
       <h1>Your library to everything.</h1>
-      <h2>Explore high-quality music with Vleer a fast, simple, and reliable app, tailored to your preferences.</h2>
+      <h2>
+        Explore high-quality music with Vleer a fast, simple, and reliable app,
+        tailored to your preferences.
+      </h2>
     </div>
     <div class="hero-img">
-      <img alt="hero-bg" src="~/assets/images/hero-bg.svg">
+      <img alt="hero-bg" src="~/assets/images/hero-bg.svg" />
     </div>
     <div class="hero-download">
       <Download class="download" />
       <div class="info">
         <p>{{ version }}</p>
         <p>{{ os }}</p>
-        <p id="install" @click="copyInstall" v-if="install != ''">{{ install }}</p>
+        <p id="install" @click="copyInstall" v-if="install != ''">
+          {{ install }}
+        </p>
       </div>
     </div>
   </div>
@@ -25,7 +30,10 @@
       <div class="wrapper">
         <div class="image-outer">
           <div class="image-inner">
-            <img alt="image" loading="lazy" src="~/assets/images/image.webp">
+            <div class="image-content">
+              
+            </div>
+            <img alt="image" loading="lazy" src="~/assets/images/image.webp" />
           </div>
         </div>
       </div>
@@ -46,93 +54,108 @@
     </div>
   </div>
   <div class="footer">
-    <img alt="footer-image" src="~/assets/images/footer.svg">
+    <img alt="footer-image" src="~/assets/images/footer.svg" />
   </div>
 </template>
 
 <script>
-import { $fetch } from 'ofetch';
+import { $fetch } from "ofetch";
 
 export default {
   data() {
     return {
       version: "v0.0.0",
       os: "os 0+",
-      install: ""
-    }
+      install: "",
+    };
   },
   methods: {
     async init() {
-      const os = window.navigator.userAgent.toLowerCase().includes('mac') ? 'macos' :
-      window.navigator.userAgent.toLowerCase().includes('win') ? 'windows' :
-      window.navigator.userAgent.toLowerCase().includes('linux') ? 'linux' : 'linux';
-    
-      
-      this.os = os == 'macos' ? "macOS 10.15+" : os == 'windows' ? "win 7+" : "webkit2gtk 4.1+";
-      
+      const os = window.navigator.userAgent.toLowerCase().includes("mac")
+        ? "macos"
+        : window.navigator.userAgent.toLowerCase().includes("win")
+        ? "windows"
+        : window.navigator.userAgent.toLowerCase().includes("linux")
+        ? "linux"
+        : "linux";
+
+      this.os =
+        os == "macos"
+          ? "macOS 10.15+"
+          : os == "windows"
+          ? "win 7+"
+          : "webkit2gtk 4.1+";
+
       /////////////
-      
+
       if (os == "macos") {
-        this.install = "Install with homebrew"
+        this.install = "Install with homebrew";
       }
-      
+
       /////////////
-      
-      const version = await $fetch('/api/latest');
+
+      const version = await $fetch("/api/latest");
       this.version = version.version;
     },
     copyInstall() {
       if (this.install == "") return;
 
-      
-      const os = window.navigator.userAgent.toLowerCase().includes('mac') ? 'macos' :
-      window.navigator.userAgent.toLowerCase().includes('win') ? 'windows' :
-      window.navigator.userAgent.toLowerCase().includes('linux') ? 'linux' : 'linux';
+      const os = window.navigator.userAgent.toLowerCase().includes("mac")
+        ? "macos"
+        : window.navigator.userAgent.toLowerCase().includes("win")
+        ? "windows"
+        : window.navigator.userAgent.toLowerCase().includes("linux")
+        ? "linux"
+        : "linux";
 
       if (os == "macos") {
-        const codeToCopy = "brew tap 0PandaDEV/repo\nbrew install --cask vleer@beta";
+        const codeToCopy =
+          "brew tap 0PandaDEV/repo\nbrew install --cask vleer@beta";
         navigator.clipboard.writeText(codeToCopy);
       }
-    }
+    },
   },
   async mounted() {
     await this.init();
-  }
-}
+  },
+};
 </script>
 
 <script setup>
 useSeoMeta({
-  title: 'Vleer - Your library to everything.',
-  description: 'Explore high-quality music with Vleer a fast, simple, and reliable app, tailored to your preferences.',
-  ogTitle: 'Vleer - Your library to everything.',
-  ogDescription: 'Explore high-quality music with Vleer a fast, simple, and reliable app, tailored to your preferences.',
-  ogImage: '/logo.png',
-  ogUrl: 'https://vleer.app/',
-  twitterTitle: 'Vleer - Your library to everything.',
-  twitterDescription: 'Explore high-quality music with Vleer a fast, simple, and reliable app, tailored to your preferences.',
-  twitterImage: '/logo.png',
-  twitterCard: 'summary'
-})
+  title: "Vleer - Your library to everything.",
+  description:
+    "Explore high-quality music with Vleer a fast, simple, and reliable app, tailored to your preferences.",
+  ogTitle: "Vleer - Your library to everything.",
+  ogDescription:
+    "Explore high-quality music with Vleer a fast, simple, and reliable app, tailored to your preferences.",
+  ogImage: "/logo.png",
+  ogUrl: "https://vleer.app/",
+  twitterTitle: "Vleer - Your library to everything.",
+  twitterDescription:
+    "Explore high-quality music with Vleer a fast, simple, and reliable app, tailored to your preferences.",
+  twitterImage: "/logo.png",
+  twitterCard: "summary",
+});
 
 useHead({
   htmlAttrs: {
-    lang: 'en'
+    lang: "en",
   },
   link: [
     {
-      rel: 'icon',
-      type: 'image/ico',
-      href: '/favicon.ico'
+      rel: "icon",
+      type: "image/ico",
+      href: "/favicon.ico",
     },
     {
-      rel: 'canonical',
-      href: 'https://vleer.app/',
-    }
-  ]
-})
+      rel: "canonical",
+      href: "https://vleer.app/",
+    },
+  ],
+});
 </script>
 
 <style lang="scss">
-@import '~/assets/css/index.scss';
+@import "~/assets/css/index.scss";
 </style>
